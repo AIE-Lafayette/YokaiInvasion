66eeaa6de107ba2289de1bc06bbede9c00a00e89
ravis <ravissavoy49@gmail.com>
2022-05-04 17:01:20 -0500

    added player input and projectile behaviour

---
+++ Assets/Scripts/Input/PlayerInputBehaviour.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInputBehaviour : MonoBehaviour
+{
+    private MovementBehaviourScript _playerMovement;
+    [SerializeField]
+    //private FireBehaviourScript _gun;
+
+    private void Awake()
+    {
+        _playerMovement = GetComponent<MovementBehaviourScript>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //lets the player move only left or right
+        _playerMovement.MoveDirection = new Vector3(Input.GetAxisRaw("Horizontal"), 0, 0).normalized;
+
+        //throws a projectitle when space is pressed
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            //_gun.Fire();
+        }
+    }
+}
