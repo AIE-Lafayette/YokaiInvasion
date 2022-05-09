eac056988522ad977ead298b31c9b4e933cec1e3
Asiko6474 <Zacharycgrippo@gmail.com>
2022-05-09 10:17:18 -0500

    Fixed graphical errors

--- Assets/Scripts/Input/InputBehavior.cs
+++ Assets/Scripts/Input/InputBehavior.cs
@@ -6,6 +6,8 @@ public class InputBehavior : MonoBehaviour
 {
 
     private PlayerMovement _playerMovement;
+    [SerializeField]
+    //private FireBehaviourScript _gun;
 
     // Start is called before the first frame update
     void Start()
@@ -18,5 +20,11 @@ public class InputBehavior : MonoBehaviour
     {
         //new Vector3(Input.GetAxisRaw("Horizontal")
         _playerMovement.MoveDirection = new Vector3(Input.GetAxisRaw("Horizontal"), 0, 0).normalized;
+
+        //throws a projectitle when space is pressed
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            //_gun.Fire();
+        }
     }
 }
