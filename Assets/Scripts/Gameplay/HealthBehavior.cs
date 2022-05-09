f3f54e0a5da43167af82c1a4d284763c9892eb42
Asiko6474 <Zacharycgrippo@gmail.com>
2022-05-09 12:10:34 -0500

    Added in enemy movement and another enemy prefab

--- Assets/Scripts/Gameplay/HealthBehavior.cs
+++ Assets/Scripts/Gameplay/HealthBehavior.cs
@@ -8,20 +8,24 @@ public class HealthBehavior : MonoBehaviour
     [SerializeField]
     int health;
 
+    //on collision
     private void OnTriggerEnter(Collider other)
     {
+        //lower health by one
         health--;
     }
 
     private void Update()
     {
-        if(Input.GetKeyDown(KeyCode.Space))
-        {
-            health--;
-        }
+        //if(Input.GetKeyDown(KeyCode.Space))
+        //{
+        //    health--;
+        //}
 
+        //If health reaches 0 then reload the scene
         if (health <= 0)
         {
+            //reload the scene
             SceneManager.LoadScene("ZachTestScene");
         }
     }
