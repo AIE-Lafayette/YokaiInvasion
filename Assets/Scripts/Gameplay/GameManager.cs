f2eea28f44c7dfd6abeb1486a8f103805d7e3d06
jasper <s218005@students.aie.edu>
2022-05-11 15:32:03 -0500

    made the enemy count and game manager

--- Assets/Scripts/Gameplay/GameManager.cs
+++ Assets/Scripts/Gameplay/GameManager.cs
@@ -1,9 +1,11 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.UI;
 public class GameManager : MonoBehaviour
 {
+    private GameObject[] _enemies;
+    public Text _enemycount;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +15,8 @@ public class GameManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        _enemies = GameObject.FindGameObjectsWithTag("Enemy");
+        _enemycount.text = "Enemies: " + _enemies.Length.ToString();
     }
+
 }
