5b4242214ddac56a3bdbb1242df9500896939487
jasper <s218005@students.aie.edu>
2022-05-11 10:16:50 -0500

    made the enemy behaviour do all of enemy movement.
    and changed the prefabs so they take the enemy behaviour and health skrip

--- Assets/Scripts/Enemy/EnemyBehaviour.cs
+++ Assets/Scripts/Enemy/EnemyBehaviour.cs
@@ -9,6 +9,19 @@ public class EnemyBehaviour : MonoBehaviour
     [SerializeField]
     private float _damage;
     private HealthBehavior _healthBehavior;
+    private Vector3 _moveDirection;
+    [SerializeField]
+    private int _speed;
+
+    /// <summary>
+    /// The speed and direction of the game object.
+    /// </summary>
+    public Vector3 MoveDirection
+    {
+        get { return _moveDirection; }
+        set { _moveDirection = value; }
+    }
+
     private void Start()
     {
         
@@ -16,7 +29,12 @@ public class EnemyBehaviour : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-
+        //makes movedirection into a new vecotor3 that uses speed to move
+        MoveDirection = new Vector3(0, 0, -_speed);
+        //addes the move direction scaled up by  the deltatime to position
+        transform.position += MoveDirection * Time.deltaTime;
+        //makes the rigidbodys velocity equal to the forward
+        GetComponent<Rigidbody>().velocity = transform.forward;
     }
     /// <summary>
     /// called when the posistion of the other and the transform of the enemy is the same
