99d410410d70e019f78c51f4c93c108ed9a40cf0
ravis <ravissavoy49@gmail.com>
2022-05-13 10:39:18 -0500

    Added an environment behavior and spawner

--- Assets/Scripts/Gameplay/EnviromentSpawnerBehavior.cs
+++ Assets/Scripts/Gameplay/EnviromentSpawnerBehavior.cs
@@ -4,15 +4,25 @@ using UnityEngine;
 
 public class EnviromentSpawnerBehavior : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField]
+    private GameObject _enviromentRef;
+    [SerializeField]
+    private float _lifeTime;
+    private float _timer;
 
     // Update is called once per frame
     void Update()
     {
-        
+        //The higher the timer the long it takes to spawn
+        _timer += Time.deltaTime;
+
+        //only spawns when the timer is greater than lifeTime
+        if (_timer >= _lifeTime)
+        {
+            //instatiats the enviroment and the position that it spawns, and its rotation.
+            GameObject enviroment = Instantiate(_enviromentRef.gameObject, transform.position, transform.rotation);
+            //resets timer
+            _timer = 0;
+        }
     }
 }
