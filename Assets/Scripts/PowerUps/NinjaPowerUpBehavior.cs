de183bb11a2d16f3d90f14c2bc3995564b8ceefe
ravis <ravissavoy49@gmail.com>
2022-05-18 16:58:56 -0500

    the ninja can be removed and added

--- Assets/Scripts/PowerUps/NinjaPowerUpBehavior.cs
+++ Assets/Scripts/PowerUps/NinjaPowerUpBehavior.cs
@@ -7,7 +7,6 @@ public class NinjaPowerUpBehavior : UsePowerUpBehavior
 {
     [SerializeField]
     private GameObject _ninjaRef;
-    private bool _active;
 
     public GameObject NinjaRef
     {
@@ -17,13 +16,11 @@ public class NinjaPowerUpBehavior : UsePowerUpBehavior
 
     private void Awake()
     {
-        Activate();
     }
 
     private void Start() 
     {
-        //_ninjaRef.SetActive(true);
-        SummonHelper();
+        Activate();
     }
 
     /// <summary>
@@ -31,34 +28,11 @@ public class NinjaPowerUpBehavior : UsePowerUpBehavior
     /// </summary>
     public override void Activate(params object[] arguments)
     {
-        if (_active == true)
-        {
             // Creates an instance of the Routine Behavior or copies the instance of it.
             RoutineBehaviour routineBehavior = RoutineBehaviour.Instance;
             //instanates the ninja 
-
-            //_ninjaRef.SetActive(true);       
+            _ninjaRef.SetActive(false);       
             // Attempts to set up a timed action where the powerup will be set back to inactive.
-            routineBehavior.StartNewTimedAction(arguments => _ninjaRef.SetActive(false), TimedActionCountType.SCALEDTIME, 2.0f);
-        }
-        
-    }
-
-    private void SummonHelper()
-    {
-        if (_active == false)
-        {
-            //instatiats the bullet and the position that it spawns, and its rotation.
-            GameObject helper = Instantiate(_ninjaRef.gameObject, transform.position, transform.rotation);
-            //helper.transform.SetParent(Owner.transform);
-            _active = true;
-        }
-
-        ////once its not active removes the helper
-        //if (_ninjaRef.activeSelf == false)
-        //{
-        //    _active = false;
-        //    Destroy(_ninjaRef);
-        //}
+            routineBehavior.StartNewTimedAction(arguments => _ninjaRef.SetActive(true), TimedActionCountType.SCALEDTIME, 5.0f);
     }
 }
