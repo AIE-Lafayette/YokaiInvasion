fbed7c205dd9d7300cd4cd5a3edf6b5bf3cae15b
ravis <ravissavoy49@gmail.com>
2022-05-18 12:45:01 -0500

    Added so the player can get activate its power up

--- Assets/Scripts/PowerUps/NinjaPowerUpBehavior.cs
+++ Assets/Scripts/PowerUps/NinjaPowerUpBehavior.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,7 +6,6 @@ using UnityEngine;
 public class NinjaPowerUpBehavior : UsePowerUpBehavior
 {
 
-    private UsePowerUpBehavior _abilityScript;
     [SerializeField]
     private GameObject _ninja;
 
@@ -17,7 +17,7 @@ public class NinjaPowerUpBehavior : UsePowerUpBehavior
 
     private void Awake()
     {
-        _abilityScript.CurrentPowerUp = new NinjaPowerUpBehavior();
+        Activate();
     }
 
     /// <summary>
@@ -27,8 +27,21 @@ public class NinjaPowerUpBehavior : UsePowerUpBehavior
     {
         // Creates an instance of the Routine Behavior or copies the instance of it.
         RoutineBehaviour routineBehavior = RoutineBehaviour.Instance;
+        //instanates the ninja 
+        SummonHelper();
         _ninja.SetActive(true);
         // Attempts to set up a timed action where the powerup will be set back to inactive.
-        routineBehavior.StartNewTimedAction(arguments => _ninja.SetActive(false), TimedActionCountType.SCALEDTIME, 0.5f);
+        routineBehavior.StartNewTimedAction(arguments => _ninja.SetActive(false), TimedActionCountType.SCALEDTIME, 10.0f);
+    }
+
+    private void SummonHelper()
+    {
+
+
+        //once its not active removes the helper
+        if (_ninja.activeSelf == false)
+        {
+            Destroy(_ninja);
+        }
     }
 }
