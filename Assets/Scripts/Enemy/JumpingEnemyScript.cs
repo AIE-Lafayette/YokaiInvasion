8857f19f0696d09f527f968a968cd727fd55120b
Asiko6474 <Zacharycgrippo@gmail.com>
2022-05-25 14:15:07 -0500

    Broken edits to jumping enemy, using lerp

--- Assets/Scripts/Enemy/JumpingEnemyScript.cs
+++ Assets/Scripts/Enemy/JumpingEnemyScript.cs
@@ -4,41 +4,41 @@ using UnityEngine;
 
 public class JumpingEnemyScript : EnemyBehaviour
 {
-    private Rigidbody _rigidbody;
-    private Vector3 _jump;
-    public bool _isGrounded = true;
     [SerializeField]
-    private float _jumpForce;
+    private Vector3 _offset;
+    private Vector3 _startPos;
+    private Vector3 _move;
+
     [SerializeField]
     private float _direction;
+    private float  _lerpTime;
 
-    public Vector3 Jump
-    {
-        get { return _jump; }
-        set { _jump = value; }
-    }
+    private bool _isGrounded = false;
 
     private void Start()
     {
-        _rigidbody = GetComponent<Rigidbody>();
-        Jump = new Vector3(0.0f, _jumpForce, - _direction);
+        _startPos = transform.position;
     }
 
-    private void OnTriggerStay(Collider other)
+    private void OnCollisionStay(Collision collision)
     {
         _isGrounded = true;
     }
 
     private  void Update()
     {
+        transform.position = Vector3.Lerp(_startPos, _startPos + _offset, Mathf.Cos(Time.time) * 0.5f + 0.5f);
+
         if (_isGrounded == true)
         {
-            //RoutineBehaviour.Instance.StartNewTimedAction(
-            //    args =>_rigidbody.AddForce(Jump * _jumpForce, ForceMode.Impulse),
-            //    TimedActionCountType.SCALEDTIME, 
-            //    1);
-            _rigidbody.AddForce(Jump * _jumpForce, ForceMode.Impulse);
             _isGrounded = false;
         }
+        //if (_isGrounded == false)
+        //{
+        //    _move = new Vector3(0, 0, -_direction);
+        //    transform.position += _move * Time.deltaTime;
+        //    GetComponent<Rigidbody>().velocity = transform.forward;
+        //}
+            
     }
 }
--- Assets/Scripts/Gameplay/Input/PlayerControls.cs
+++ Assets/Scripts/Gameplay/Input/PlayerControls.cs
@@ -1,4 +1,4 @@
-// GENERATED AUTOMATICALLY FROM 'Assets/Scripts/Input/PlayerControls.inputactions'
+// GENERATED AUTOMATICALLY FROM 'Assets/Scripts/Gameplay/Input/PlayerControls.inputactions'
 
 using System;
 using System.Collections;
